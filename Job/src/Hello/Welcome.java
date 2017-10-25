df8f1d5992e8f502264235be4f4649457731ad05
lappy <neerukondavakula@gmail.com>
2017-10-24 19:23:54 -0700

    "Updated Project"

---
+++ Job/src/Hello/Welcome.java
@@ -0,0 +1,14 @@
+package Hello;
+
+public class Welcome {
+
+	public static void firstMethod(){
+		System.out.println("This is my project");
+	}
+	
+	public static void main(String[] args){
+		firstMethod();
+		
+	}
+	
+}
