cece2e911ddef17f523e661bf3b00c4ff94a0bc3
lappy <neerukondavakula@gmail.com>
2017-10-24 20:00:09 -0700

    "First Job"

--- Job/src/Hello/Welcome.java
+++ Job/src/Hello/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void firstMethod(){
 		System.out.println("This is my project");
 		System.out.println("Second Commit");
+		System.out.println("First Job");
 	}
 	
 	public static void main(String[] args){
